c909afc9100f34959d04061cb823ee4d93659427
Anuska Magar <anuskamagarrr@gmail.com>
2026-05-17 17:21:28 +0545

    Update DriverListServlet

--- src/main/java/com/yatrago/driver/controller/DriverListServlet.java
+++ src/main/java/com/yatrago/driver/controller/DriverListServlet.java
@@ -1,4 +1,22 @@
 package com.yatrago.driver.controller;
 
-public class DriverListServlet {
+import com.yatrago.driver.dao.DriverDAO;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/admin/driver-list")
+public class DriverListServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        DriverDAO driverDAO = new DriverDAO();
+        req.setAttribute("drivers", driverDAO.getAllDrivers());
+        req.getRequestDispatcher("/WEB-INF/admin/drivers/driver-list.jsp").forward(req, resp);
+    }
 }
